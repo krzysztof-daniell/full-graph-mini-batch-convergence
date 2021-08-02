93dadd0095955d3c339e06a18bae08546122de38
ksadowski13 <krzysztof.sadowski@intel.com>
2021-08-02 15:32:08 +0200

    finished rgcn homo ns

--- models/rgcn_homo_ns.py
+++ models/rgcn_homo_ns.py
@@ -8,6 +8,53 @@ import torch
 import torch.nn as nn
 
 
+class RelGraphEmbedLayer(nn.Module):
+    def __init__(
+        self,
+        node_tids: torch.Tensor,
+        num_ntypes: int,
+        input_size: list[Union[int, None]],
+        embedding_size: int,
+    ):
+        super().__init__()
+        self._num_ntypes = num_ntypes
+        self._embedding_size = embedding_size
+
+        self.embeddings = nn.ParameterDict()
+
+        for ntype in range(num_ntypes):
+            if input_size[ntype] is not None:
+                embedding = nn.Parameter(torch.Tensor(
+                    input_size[ntype].shape[-1], embedding_size))
+                nn.init.xavier_uniform_(embedding)
+
+                self.embeddings[f'{ntype}'] = embedding
+
+        self.node_embeddings = nn.Embedding(
+            node_tids.shape[0], embedding_size, sparse=True)
+        nn.init.uniform_(self.node_embeddings.weight, -1, 1)
+
+    def forward(
+        self,
+        node_ids: torch.Tensor,
+        node_tids: torch.Tensor,
+        type_ids: torch.Tensor,
+        inputs: torch.Tensor,
+    ) -> torch.Tensor:
+        x = torch.empty(node_ids.shape[0], self._embedding_size)
+
+        for ntype in range(self._num_ntypes):
+            loc = node_tids == ntype
+
+            if inputs[ntype] is not None:
+                x[loc] = inputs[ntype][type_ids[loc]
+                                       ] @ self.embeddings[f'{ntype}']
+            else:
+                x[loc] = self.node_embeddings(node_ids[loc])
+
+        return x
+
+
 class EntityClassify(nn.Module):
     def __init__(
         self,
@@ -27,48 +74,48 @@ class EntityClassify(nn.Module):
         self._layers = nn.ModuleList()
 
         self._layers.append(dglnn.RelGraphConv(
-            hidden_feats, 
-            hidden_feats, 
-            num_rels, 
-            'basis', 
-            num_bases, 
-            activation=activation, 
-            self_loop=self_loop, 
-            low_mem=True, 
-            dropout=dropout, 
+            hidden_feats,
+            hidden_feats,
+            num_rels,
+            'basis',
+            num_bases,
+            activation=activation,
+            self_loop=self_loop,
+            low_mem=True,
+            dropout=dropout,
             layer_norm=layer_norm,
         ))
 
         for _ in range(1, num_layers - 1):
             self._layers.append(dglnn.RelGraphConv(
-                hidden_feats, 
-                hidden_feats, 
-                num_rels, 
-                'basis', 
-                num_bases, 
-                activation=activation, 
-                self_loop=self_loop, 
-                low_mem=True, 
-                dropout=dropout, 
+                hidden_feats,
+                hidden_feats,
+                num_rels,
+                'basis',
+                num_bases,
+                activation=activation,
+                self_loop=self_loop,
+                low_mem=True,
+                dropout=dropout,
                 layer_norm=layer_norm,
             ))
 
         self._layers.append(dglnn.RelGraphConv(
-            hidden_feats, 
-            out_feats, 
-            num_rels, 
-            'basis', 
-            num_bases, 
-            activation=None, 
-            self_loop=self_loop, 
-            low_mem=True, 
-            dropout=dropout, 
+            hidden_feats,
+            out_feats,
+            num_rels,
+            'basis',
+            num_bases,
+            activation=None,
+            self_loop=self_loop,
+            low_mem=True,
+            dropout=dropout,
             layer_norm=layer_norm,
         ))
 
     def forward(
-        self, 
-        blocks: dgl.DGLHeteroGraph, 
+        self,
+        blocks: dgl.DGLGraph,
         inputs: torch.Tensor,
     ) -> torch.Tensor:
         x = inputs
@@ -78,62 +125,150 @@ class EntityClassify(nn.Module):
 
         return x
 
-
-class RelGraphEmbedLayer(nn.Module):
-    def __init__(
+    def inference(
         self,
-        num_nodes: int,
-        num_ntypes: int,
-        input_size: list[Union[int, None]],
-        embedding_size: int,
-    ):
-        super().__init__()
-        self._num_ntypes = num_ntypes
-        self._embedding_size = embedding_size
-
-        self._embeddings = nn.ParameterDict()
-        self._node_embeddings = {}
-
-        for ntype in range(num_ntypes):  # TODO: make logic from benchmarks
-            if isinstance(input_size[ntype], int):
-                # self._node_embeddings[f'{ntype}'] = dglnn.NodeEmbedding(
-                #     input_size[ntype], 
-                #     embedding_size, 
-                #     name=f'{ntype}', 
-                #     init_func=self._initializer,
-                # )
-                node_embedding = nn.Embedding(input_size[ntype], embedding_size, sparse=True)
-                nn.init.uniform_(node_embedding.weight, -1, 1)
-
-                self._node_embeddings[f'{ntype}'] = node_embedding
-            else:
-                embedding = nn.Parameter(torch.empty(input_size[ntype].shape[-1], embedding_size))
-                nn.init.xavier_uniform_(embedding)
-
-                self._embeddings[f'{ntype}'] = embedding
-
-    def _initializer(embedding: torch.Tensor):
-        embedding.uniform_(-1, 1)
-
-        return embedding
-
-    def forward(
-        self, 
-        node_ids: torch.Tensor, 
-        ntype_ids: torch.Tensor, 
+        g: dgl.DGLGraph,
         inputs: torch.Tensor,
     ) -> torch.Tensor:
-        embeddings = torch.empty(node_ids.shape[0], self._embedding_size)
-        locs = [None for _ in range(self._num_ntypes)]
+        x = inputs
 
-        for ntype in range(self._num_ntypes):
-            locs[ntype] = (ntype_ids == ntype).nonzero().squeeze(dim=-1)
-            
-        for ntype in range(self._num_ntypes):
-            loc = ntype_ids == ntype
+        for layer in self._layers:
+            x = layer(g, x, g.edata['etype'], g.edata['norm'])
+            # x = layer(g, x, g.edata[dgl.ETYPE], g.edata['norm'])  # TODO: try this version
 
-            if isinstance(inputs[ntype], int):
-                embeddings[loc] = self._node_embeddings[f'{ntype}']()
+        return x
 
 
+def train(
+    embedding_layer: nn.Module,
+    model: nn.Module,
+    embedding_optimizer: torch.optim.Optimizer,
+    model_optimizer: torch.optim.Optimizer,
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    node_feats: torch.Tensor,
+    labels: torch.Tensor,
+    dataloader: dgl.dataloading.NodeDataLoader,
+) -> tuple[float]:
+    embedding_layer.train()
+    model.train()
 
+    total_loss = 0
+    total_accuracy = 0
+
+    start = default_timer()
+
+    for step, (in_nodes, _, blocks) in enumerate(dataloader):
+        embedding_optimizer.zero_grad()
+        model_optimizer.zero_grad()
+
+        batch_labels = labels[blocks[-1].dstdata['type_id']]
+
+        inputs = embedding_layer(
+            in_nodes,
+            blocks[0].srcdata['ntype'],
+            blocks[0].srcdata['type_id'],
+            node_feats,
+        )
+        logits = model(blocks, inputs)
+        loss = loss_function(logits, batch_labels)
+
+        loss.backward()
+        model_optimizer.step()
+        embedding_optimizer.step()
+
+        _, indices = torch.max(logits, dim=1)
+        correct = torch.sum(indices == batch_labels)
+        accuracy = correct.item() / len(batch_labels)
+
+        total_loss += loss.item()
+        total_accuracy += accuracy
+
+    stop = default_timer()
+    time = stop - start
+
+    total_loss /= step + 1
+    total_accuracy /= step + 1
+
+    return time, total_loss, total_accuracy
+
+
+def validate_full(
+    embedding_layer: nn.Module,
+    model: nn.Module,
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    node_feats: torch.Tensor,
+    labels: torch.Tensor,
+    g: dgl.DGLGraph,
+    mask: torch.Tensor,
+) -> tuple[float]:
+    embedding_layer.eval()
+    model.eval()
+
+    inference_labels = labels[mask]
+
+    start = default_timer()
+
+    with torch.no_grad():
+        inputs = embedding_layer(
+            g.srcdata[dgl.NID],
+            # g.srcdata[dgl.NTYPE],
+            g.srcdata['ntype'],
+            g.srcdata['type_id'],
+            node_feats,
+        )
+        logits = model.inference(g, inputs)[mask]
+        loss = loss_function(logits, inference_labels)
+
+    _, indices = torch.max(logits, dim=1)
+    correct = torch.sum(indices == inference_labels)
+    accuracy = correct.item() / len(inference_labels)
+
+    stop = default_timer()
+    time = stop - start
+
+    return time, loss, accuracy
+
+
+def validate(
+    embedding_layer: nn.Module,
+    model: nn.Module,
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    node_feats: torch.Tensor,
+    labels: torch.Tensor,
+    dataloader: dgl.dataloading.NodeDataLoader,
+) -> tuple[float]:
+    embedding_layer.eval()
+    model.eval()
+
+    total_loss = 0
+    total_accuracy = 0
+
+    start = default_timer()
+
+    with torch.no_grad():
+        for step, (_, _, blocks) in enumerate(dataloader):
+            batch_labels = labels[blocks[-1].dstdata['type_id']]
+
+            inputs = embedding_layer(
+                blocks[0].srcdata[dgl.NID],
+                blocks[0].srcdata[dgl.NTYPE],
+                blocks[0].srcdata['type_id'],
+                node_feats,
+            )
+            logits = model(blocks, inputs)
+            loss = loss_function(logits, batch_labels)
+
+            _, indices = torch.max(logits, dim=1)
+            correct = torch.sum(indices == batch_labels)
+            accuracy = correct.item() / len(batch_labels)
+
+            total_loss += loss.item()
+            total_accuracy += accuracy
+
+    stop = default_timer()
+    time = stop - start
+
+    total_loss /= step + 1
+    total_accuracy /= step + 1
+
+    return time, loss, accuracy
