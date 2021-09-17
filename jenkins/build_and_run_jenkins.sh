61cc5f5bf91bc6822d9f7167de4f262531298708
ksadowski13 <krzysztof.sadowski@intel.com>
2021-09-17 13:26:44 +0200

    fix for volumes

--- jenkins/build_and_run_jenkins.sh
+++ jenkins/build_and_run_jenkins.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-mkdir -p /home/jenkins/volumes/dgl_sigopt_jenkins_certs
-mkdir -p /home/jenkins/volumes/dgl_sigopt_jenkins_home
-
 docker build -t dgl-sigopt-jenkins-image .
 docker run \
   -d \
   -it \
-  --volume /home/jenkins/volumes/jenkins_certs:/certs/client \
-  --volume /home/jenkins/volumes/jenkins_home:/var/jenkins_home \
+  --volume /home/jenkins/volumes/dgl_sigopt_jenkins_certs:/certs/client \
+  --volume /home/jenkins/volumes/dgl_sigopt_jenkins_home:/var/jenkins_home \
   --publish 8080:8080 \
   --publish 50000:50000 \
   --name dgl-sigopt-jenkins \
-  dgl-sigopt-jenkins-image
+  dgl-sigopt-jenkins-image:latest
