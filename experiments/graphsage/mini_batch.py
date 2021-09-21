1544d15eb0b86e42c646fa5ec3484a9f855ea99a
ksadowski13 <krzysztof.sadowski@intel.com>
2021-09-21 19:34:38 +0200

    fix for failed run

--- experiments/graphsage/mini_batch.py
+++ experiments/graphsage/mini_batch.py
@@ -364,6 +364,9 @@ if __name__ == '__main__':
 
         while not experiment.is_finished():
             with experiment.create_run() as sigopt_context:
-                run(args, sigopt_context=sigopt_context)
+                try:
+                    run(args, sigopt_context=sigopt_context)
+                except:
+                    sigopt_context.log_failure()
     else:
         run(args)
